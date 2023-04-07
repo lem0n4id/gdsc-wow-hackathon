7d202f6c90958c39e9e2dacb406c85367dd567f4
Lenin Kennedy <leninkennedy4work@gmail.com>
2023-04-07 12:22:54 +0000

    fix: handle unselected options

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ def parse_form_data(form_data):
     parsed_data = {}
     for key, value in form_data.items():
         if key != 'name':
-            parsed_value = float(value.split(' ')[-1][1:-1])
-            parsed_data[key] = parsed_value
+            try:
+                parsed_value = float(value.split(' ')[-1][1:-1])
+                parsed_data[key] = parsed_value
+            except:
+                parsed_data[key] = 0
     return parsed_data
